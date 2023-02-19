a6ae31e187868fd4164c7fd9df695c5d2afa76b3
Robby Kurniawan <thelazpiz@gmail.com>
2023-02-19 10:05:40 +0700

    navbar

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,16 +1,44 @@
 import { Container, Nav, Navbar } from "react-bootstrap";
+import { Link, useNavigate } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faRightFromBracket } from "@fortawesome/free-solid-svg-icons";
 
 export default function NavBar() {
+	const navigate = useNavigate();
+	const handleLogout = () => {
+		localStorage.clear();
+		navigate("/login");
+	};
 	return (
 		<>
-			<Navbar bg="light" variant="light">
-				<Container>
-					<Navbar.Brand href="#home">Navbar</Navbar.Brand>
-					<Nav className="me-auto">
-						<Nav.Link href="#home">Home</Nav.Link>
-						<Nav.Link href="#features">Features</Nav.Link>
-						<Nav.Link href="#pricing">Pricing</Nav.Link>
-					</Nav>
+			<Navbar className="NavBar-BG" sticky="top">
+				<Container className="NavBar">
+					<div>
+						<Link to="/" className="nav-link">
+							<b className="nav-text">ORDER MANAGEMENT</b>
+						</Link>
+					</div>
+					<div>
+						<Nav
+							style={{
+								gap: "50px",
+								alignItems: "center",
+							}}
+						>
+							<Link to="/" className="nav-link">
+								<div className="nav-text">ORDER LIST</div>
+							</Link>
+							<Link to="/login" className="nav-link" onClick={handleLogout}>
+								<div className="nav-logout">
+									<div className="nav-text">LOGOUT</div>
+									<FontAwesomeIcon
+										icon={faRightFromBracket}
+										className="nav-text"
+									/>
+								</div>
+							</Link>
+						</Nav>
+					</div>
 				</Container>
 			</Navbar>
 		</>
